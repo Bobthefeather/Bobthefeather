395891a763164418af274f55060091c7826406c0
heidi <57692@fairholme.qld.edu.au>
2024-08-13 11:09:16 +1000

    starter

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, your name goes here!'
+
+@app.route('/shop')
+def home():
+    return 'this is not your home page!'
+
+if __name__=='__main__':
+    app.run()
+
+bgdggb
